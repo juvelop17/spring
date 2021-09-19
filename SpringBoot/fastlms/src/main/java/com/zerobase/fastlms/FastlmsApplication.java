a9a6e2949f96992409ecaa4cad1a76ce1ca85f3f
Junho Kim <juvelop17@gmail.com>
2021-09-19 13:40:42 +0900

    project initiated.

--- SpringBoot/fastlms/src/main/java/com/zerobase/fastlms/FastlmsApplication.java
+++ SpringBoot/fastlms/src/main/java/com/zerobase/fastlms/FastlmsApplication.java
@@ -12,10 +12,10 @@ public class FastlmsApplication {
     /*
     @PostConstruct
     public void init() {
-    
+
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         System.out.println("Spring boot application running in UTC timezone :" + new Date());
-        
+
     }*/
     
 
