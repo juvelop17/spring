3ac8dd46919d9f54994a42e318d6f1f030f9eea0
Junho Kim <juvelop17@gmail.com>
2021-11-01 19:04:41 +0900

    minor changes

--- SpringBoot/Assignment/week11/zerobase-homework_11_week/src/test/java/com/example/demo/BookStockTest.java
+++ SpringBoot/Assignment/week11/zerobase-homework_11_week/src/test/java/com/example/demo/BookStockTest.java
@@ -1,7 +1,18 @@
 package com.example.demo;
 
+import com.example.demo.exception.ExceptionCode;
+import com.example.demo.exception.ZeroBaseException;
+import com.example.demo.infra.entity.BookStock;
+import com.example.demo.infra.entity.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 public class BookStockTest {
 
@@ -9,11 +20,19 @@ public class BookStockTest {
     @DisplayName("BookStock 재고가 정상적으로 차감된다")
     void success__decreaseStock() {
         // FIXME
+        final int stockCount = 10;
+        BookStock bookStock = BookStock.builder().stock(stockCount).build();
+        bookStock.decreaseStock();
+        assertThat(bookStock.getStock()).isEqualTo(stockCount-1);
     }
 
     @Test
     @DisplayName("BookStock 재고가 0일때, 차감을 시도하면 익셉션 발생(NOT_ENOUGH_STOCK)")
     void throwException__when__not_enough_stock() {
         // FIXME
+        BookStock bookStock = BookStock.builder().stock(0).build();
+        assertThatThrownBy(bookStock::decreaseStock)
+                .isInstanceOf(ZeroBaseException.class)
+                .hasFieldOrPropertyWithValue("code", ExceptionCode.NOT_ENOUGH_STOCK);
     }
 }
--- SpringBoot/Assignment/week11/zerobase-homework_11_week/src/test/java/com/example/demo/OrderServiceTest.java
+++ SpringBoot/Assignment/week11/zerobase-homework_11_week/src/test/java/com/example/demo/OrderServiceTest.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.exception.ExceptionCode;
+import com.example.demo.exception.ZeroBaseException;
+import com.example.demo.infra.entity.Book;
+import com.example.demo.infra.entity.BookOrder;
+import com.example.demo.infra.entity.BookStock;
+import com.example.demo.infra.entity.User;
 import com.example.demo.infra.repository.BookOrderRepository;
 import com.example.demo.infra.repository.BookRepository;
 import com.example.demo.infra.repository.BookStockRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +18,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -27,33 +44,80 @@ class OrderServiceTest {
     @InjectMocks
     OrderService orderService;
 
+    @BeforeEach
+    void setUp() {
+        when(bookRepository.findById(anyLong())).thenReturn(Optional.of(new Book(true, 0)));
+        when(bookStockRepository.findByBookId((anyLong()))).thenReturn(Optional.of(new BookStock(10)));
+        when(bookOrderRepository.save(any(BookOrder.class))).thenReturn(new BookOrder(1L, null, null));
+    }
+
     @Test
     @DisplayName("재고가 정상으로 차감되며 주문이 성공한다.")
     void success__order() {
         // FIXME
+        // given
+        final int stockCount = 10;
+        BookStock bookStock = BookStock.builder().stock(stockCount).build();
+        // when
+        when(bookStockRepository.findByBookId(anyLong())).thenReturn(Optional.of(bookStock));
+        orderService.order(User.asNew(1L), 1L);
+
+        assertThat(bookStock.getStock()).isLessThan(stockCount);
+
+        verify(bookRepository).findById(anyLong());
+        verify(bookStockRepository).findByBookId((anyLong()));
+        verify(bookOrderRepository).save(any(BookOrder.class));
     }
 
     @Test
     @DisplayName("Book 존재하지 않으면 익셉션 발생(NOT_FOUND_BOOK)")
     void throwException__when__not_found_book() {
         // FIXME
+        // given when
+        when(bookRepository.findById(anyLong())).thenReturn(Optional.empty());
+        // then
+        assertThatThrownBy(() -> orderService.order(User.asNew(1L), 1L))
+                .isInstanceOf(ZeroBaseException.class)
+                .hasFieldOrPropertyWithValue("code", ExceptionCode.NOT_FOUND_BOOK);
     }
 
     @Test
     @DisplayName("BookStock 존재하지 않으면 익셉션 발생(NOT_FOUND_BOOK_STOCK)")
     void throwException__when__not_found_bookstock() {
         // FIXME
+        // given when
+        when(bookStockRepository.findByBookId(anyLong())).thenReturn(Optional.empty());
+        // then
+        assertThatThrownBy(() -> orderService.order(User.asNew(1L), 1L))
+                .isInstanceOf(ZeroBaseException.class)
+                .hasFieldOrPropertyWithValue("code", ExceptionCode.NOT_FOUND_BOOK_STOCK);
     }
 
     @Test
     @DisplayName("판매중이지 않은 책을 주문하면 익셉션 발생(FAIL_BOOK_ORDER)")
     void throwException__when__book_sale_off() {
         // FIXME
+        // given
+        Book book = Book.builder().sale(false).build();
+        // when
+        when(bookRepository.findById(anyLong())).thenReturn(Optional.of(book));
+        // then
+        assertThatThrownBy(() -> orderService.order(User.asNew(1L), 1L))
+                .isInstanceOf(ZeroBaseException.class)
+                .hasFieldOrPropertyWithValue("code", ExceptionCode.BOOK_IS_NOT_SALE);
     }
 
     @Test
     @DisplayName("재고가 부족할때 익셉션 발생(FAIL_BOOK_ORDER)")
     void throwException__when__not_enough_stock() {
         // FIXME
+        // given
+        BookStock bookStock = BookStock.builder().stock(0).build();
+        // when
+        when(bookStockRepository.findByBookId(anyLong())).thenReturn(Optional.of(bookStock));
+        // then
+        assertThatThrownBy(() -> orderService.order(User.asNew(1L), 1L))
+                .isInstanceOf(ZeroBaseException.class)
+                .hasFieldOrPropertyWithValue("code", ExceptionCode.FAIL_BOOK_ORDER);
     }
 }
